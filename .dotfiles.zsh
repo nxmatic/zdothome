ab8a00aae6c52db04b9807322a9f6c72010adb95
Stephane Lacoin (aka nxmatic) <stephane.lacoin@gmail.com>
2021-10-12 20:35:59 +0200

    fix (setup): tempfile should always be in dotfiles folder

--- .dotfiles.zsh
+++ .dotfiles.zsh
@@ -1,10 +1,10 @@
 #!/usr/bin/env zsh
 
-alias dtfsh="env PS1=\"dtfsh> \" GIT_WORK_TREE=\"\$DOTFILES\" GIT_DIR=\"\$DOTFILES/.dotfiles.git\" zsh -d -f"
+alias dtfsh="env PS1=\"dtfsh> \" GIT_WORK_TREE=\"\${DOTFILES}\" GIT_DIR=\"\${DOTFILES}/.dotfiles.git\" zsh -d -f"
 
 # Installation script (Bash) for https://github.com/nxmatic/dotfiles
 
-if [ -z "$DOTFILES" ]; then
+if [ -z "${DOTFILES}" ]; then
     if [ -h /bin/ls ] && expr "$(readlink /bin/ls)" = '/bin/busybox' > /dev/null; then
         apk add coreutils
         [ -x /usr/bin/rsync ] || apk add rsync
@@ -17,19 +17,19 @@ setopt aliases
 # TODO: giconfigure sparse checkout based on os/flavors
 
 dtfsh <<EOF
-if [ ! -d "\$GIT_DIR" ]; then
-    tmpfile=\$(mktemp -d $(basename $0).XXXXX) && trap 0 "rm -fr \$tmpfile"
+if [ ! -d "\${GIT_DIR}" ]; then
+    tmpfile=\$(mktemp -d ~${DOTFILES}/$(basename $0).XXXXX) && trap 0 "rm -fr \$tmpfile"
 
     git clone --quiet --bare https://github.com/nxmatic/dotfiles "\$GIT_DIR" 
     git ls-tree -r HEAD | awk '{print \$NF}' > \$tmpfile/ls-tree
     
-    rsync -av --files-from=\$tmpfile/ls-tree \$GIT_WORK_TREE \$tmpfile 2>/dev/null || true
+    rsync -av --files-from=\${tmpfile}/ls-tree \$GIT_WORK_TREE \${tmpfile} 2>/dev/null || true
     
-    (cd \$GIT_WORK_TREE && xargs rm -f < \$tmpfile/ls-tree)
+    (cd \${GIT_WORK_TREE} && xargs rm -f < \${tmpfile}/ls-tree)
 
     git config status.showUntrackedFiles no
     git checkout
-    rsync -av --files-from=\$tmpfile/ls-tree \$tmpfile \$GIT_WORK_TREE 2>/dev/null || true
+    rsync -av --files-from=\${tmpfile}/ls-tree \$tmpfile \${GIT_WORK_TREE} 2>/dev/null || true
 else
     git fetch
 fi
